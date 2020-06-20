76dac481b2573e88dbb134108f6ac2cd5402d56f
Jacob Collins <jacobc1596@gmail.com>
2020-06-20 12:11:21 +0100

    Updated readme and JS code

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Things I've learned:
+Things I've learned & practiced:
 
 1. Checking if an element contains a specific class = `element.classList.contains('className')`.
-2. Using `oncontextmenu` to execute a function when the user right-clicks on an element with a context menu. The function receives an `event` object as its sole argument.
+2. Using `oncontextmenu` to execute a function when the user right-clicks on an element with a context menu. The function receives an `event` object as its sole argument. Also using `e.preventDefault()`.
+3. Using `setAttribute` and `getAttribute`.
+4. Using `fill()` method to populate an Array.
--- script.js
+++ script.js
@@ -56,7 +56,6 @@ function createBoard(){
 
 
             squares[i].setAttribute('data', total);
-            console.log(squares[i]);
         }
     }
 
@@ -114,49 +113,41 @@ function checkSquare(square, currentId) {
     setTimeout(() => {
       if (currentId > 0 && !isLeftEdge) {
         const newId = squares[parseInt(currentId) -1].id
-        //const newId = parseInt(currentId) - 1   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
       if (currentId > 9 && !isRightEdge) {
         const newId = squares[parseInt(currentId) +1 -width].id
-        //const newId = parseInt(currentId) +1 -width   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
       if (currentId > 10) {
         const newId = squares[parseInt(currentId -width)].id
-        //const newId = parseInt(currentId) -width   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
       if (currentId > 11 && !isLeftEdge) {
         const newId = squares[parseInt(currentId) -1 -width].id
-        //const newId = parseInt(currentId) -1 -width   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
       if (currentId < 98 && !isRightEdge) {
         const newId = squares[parseInt(currentId) +1].id
-        //const newId = parseInt(currentId) +1   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
       if (currentId < 90 && !isLeftEdge) {
         const newId = squares[parseInt(currentId) -1 +width].id
-        //const newId = parseInt(currentId) -1 +width   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
       if (currentId < 88 && !isRightEdge) {
         const newId = squares[parseInt(currentId) +1 +width].id
-        //const newId = parseInt(currentId) +1 +width   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
       if (currentId < 89) {
         const newId = squares[parseInt(currentId) +width].id
-        //const newId = parseInt(currentId) +width   ....refactor
         const newSquare = document.getElementById(newId)
         click(newSquare)
       }
